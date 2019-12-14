a9a7ebe9797e9dc160112028f627855e0fd94069
Nis Meinert <nis.meinert@gmail.com>
2019-12-14 17:07:22 +0100

    reformat files according to .clang-format

--- static_rotation.hpp
+++ static_rotation.hpp
@@ -7,52 +7,44 @@ template <typename N, typename T, T head, T... tail>
 class Rotator {
     static_assert(std::is_same_v<typename N::value_type, std::size_t>);
 
-private:
+  private:
     template <T... a, T... b>
-    [[nodiscard]] constexpr static auto join(
-        std::integer_sequence<T, a...>,
-        std::integer_sequence<T, b...>) noexcept
-    {
-        return std::integer_sequence<T, a..., b...> {};
+    [[nodiscard]] constexpr static auto join(std::integer_sequence<T, a...>,
+                                             std::integer_sequence<T, b...>) noexcept {
+        return std::integer_sequence<T, a..., b...>{};
     }
 
     template <T... values>
-    [[nodiscard]] constexpr static auto to_array(
-        std::integer_sequence<T, values...>) noexcept
-    {
-        return std::array { values... };
+    [[nodiscard]] constexpr static auto to_array(std::integer_sequence<T, values...>) noexcept {
+        return std::array{values...};
     }
 
-public:
-    using next = Rotator<std::integral_constant<typename N::value_type, N::value - 1>,
-        T,
-        tail...,
-        head>;
-    constexpr static auto seq = join(std::integer_sequence<T, head, tail...> {}, next::seq);
+  public:
+    using next
+        = Rotator<std::integral_constant<typename N::value_type, N::value - 1>, T, tail..., head>;
+    constexpr static auto seq = join(std::integer_sequence<T, head, tail...>{}, next::seq);
     constexpr static auto seq_length = sizeof...(tail) + 1;
     constexpr static auto values = to_array(seq);
 };
 
 template <typename T, T head, T... tail>
 struct Rotator<std::integral_constant<std::size_t, 0>, T, head, tail...> {
-    constexpr static auto seq = std::integer_sequence<T> {};
+    constexpr static auto seq = std::integer_sequence<T>{};
 };
 } // namespace detail
 
 template <typename T, T... values>
-using Rotations = detail::Rotator<std::integral_constant<std::size_t, sizeof...(values)>,
-    T,
-    values...>;
+using Rotations
+    = detail::Rotator<std::integral_constant<std::size_t, sizeof...(values)>, T, values...>;
 
 namespace detail {
 template <std::size_t... idxs>
-[[nodiscard]] constexpr auto make_index_rotations(
-    std::integer_sequence<std::size_t, idxs...>) noexcept
-{
-    return Rotations<std::size_t, idxs...> {};
+[[nodiscard]] constexpr auto
+make_index_rotations(std::integer_sequence<std::size_t, idxs...>) noexcept {
+    return Rotations<std::size_t, idxs...>{};
 }
 } // namespace detail
 
 template <std::size_t N>
-using IndexRotations = decltype(detail::make_index_rotations(std::make_index_sequence<N> {}));
+using IndexRotations = decltype(detail::make_index_rotations(std::make_index_sequence<N>{}));
 } // namespace static_rotation
--- test.cpp
+++ test.cpp
@@ -1,11 +1,10 @@
 #include "static_rotation.hpp"
 
-int main()
-{
+int main() {
     using namespace static_rotation;
     using R = IndexRotations<4>;
     constexpr auto values = R::values;
-    constexpr std::array<std::size_t, 16> expected {
+    constexpr std::array<std::size_t, 16> expected{
         // clang-format off
         0, 1, 2, 3,
         1, 2, 3, 0,
@@ -16,11 +15,9 @@ int main()
 
     static_assert(R::seq_length == 4);
     static_assert([values, expected]() {
-        if (values.size() != expected.size())
-            return false;
+        if (values.size() != expected.size()) return false;
         for (int i = 0; i < values.size(); i++) {
-            if (values.at(i) != expected.at(i))
-                return false;
+            if (values.at(i) != expected.at(i)) return false;
         }
         return true;
     }());
